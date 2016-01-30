af205aeda1532f89db9af4f560c9189ac8ed6e33
Jimmy Lord <jimmylord@gmail.com>
2016-01-30 11:03:51 -0500

    Defined delete key.

--- MyFramework/SourceCommon/Helpers/ExternalTools.h
+++ MyFramework/SourceCommon/Helpers/ExternalTools.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012-2015 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2016 Jimmy Lord http://www.flatheadgames.com
 //
 // This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
@@ -91,6 +91,9 @@
 #endif
 #ifndef MYKEYCODE_DOWN
 #define MYKEYCODE_DOWN          40
+#endif
+#ifndef MYKEYCODE_DELETE
+#define MYKEYCODE_DELETE        127
 #endif
 
 #if MYFW_WP8
