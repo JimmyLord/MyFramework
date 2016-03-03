790345d0c243d87d8d4f53a82d95865d3459ae2d
Jimmy Lord <jimmylord@gmail.com>
2016-03-03 08:18:36 -0500

    Added a call to stop music on Windows.

--- MyFramework/SourceWindows/SoundPlayer.cpp
+++ MyFramework/SourceWindows/SoundPlayer.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012-2015 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2016 Jimmy Lord http://www.flatheadgames.com
 //
 // This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
@@ -56,7 +56,7 @@ void SoundPlayer::OnFocusLost()
 
 void SoundPlayer::PlayMusic(char* path)
 {
-    m_Music = Mix_LoadWAV( "sound/Music.wav" );
+    m_Music = Mix_LoadWAV( path );//"sound/Music.wav" );
     Mix_PlayChannel( 0, m_Music, -1 );
 }
 
@@ -70,6 +70,7 @@ void SoundPlayer::UnpauseMusic()
 
 void SoundPlayer::StopMusic()
 {
+    Mix_HaltChannel( 0 );
 }
 
 int SoundPlayer::LoadSound(const char* path, const char* ext)
