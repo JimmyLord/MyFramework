bb091082946b74ae6160563a50f6e071de313e27
Jimmy Lord <jimmylord@gmail.com>
2016-06-03 10:35:28 -0400

    Added a set function to MyRect.

--- MyFramework/SourceCommon/DataTypes/Vector.h
+++ MyFramework/SourceCommon/DataTypes/Vector.h
@@ -238,6 +238,8 @@ public:
 public:
     MyRect() {}
     MyRect(int nx, int ny, int nw, int nh) { x = nx; y = ny; w = nw; h = nh; }
+
+    void Set(int nx, int ny, int nw, int nh) { x = nx; y = ny; w = nw; h = nh; }
 };
 
 template <class MyType> class Vector2T
