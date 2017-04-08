bb8333777c07f148b746909c0aac4359c0717aab
Jimmy Lord <jimmylord@gmail.com>
2017-04-08 11:24:06 -0400

    Fixed bug in MyFileObject::IsFinishedLoading.

--- MyFramework/SourceCommon/Helpers/MyFileObject.cpp
+++ MyFramework/SourceCommon/Helpers/MyFileObject.cpp
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012-2016 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2017 Jimmy Lord http://www.flatheadgames.com
 //
 // This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
@@ -217,10 +217,10 @@ void MyFileObject::Rename(const char* newnamewithoutextension)
 
 bool MyFileObject::IsFinishedLoading()
 {
-    if( m_FileLoadStatus < FileLoadStatus_Success )
-        return true;
+    if( m_FileLoadStatus < FileLoadStatus_Success ) // still loading
+        return false;
 
-    return false;
+    return true;
 }
 
 void MyFileObject::RegisterFileFinishedLoadingCallback(void* pObj, FileFinishedLoadingCallbackFunc pCallback)
--- MyFramework/SourceCommon/Helpers/MyFileObject.h
+++ MyFramework/SourceCommon/Helpers/MyFileObject.h
@@ -1,5 +1,5 @@
 //
-// Copyright (c) 2012-2016 Jimmy Lord http://www.flatheadgames.com
+// Copyright (c) 2012-2017 Jimmy Lord http://www.flatheadgames.com
 //
 // This software is provided 'as-is', without any express or implied warranty.  In no event will the authors be held liable for any damages arising from the use of this software.
 // Permission is granted to anyone to use this software for any purpose, including commercial applications, and to alter it and redistribute it freely, subject to the following restrictions:
@@ -35,11 +35,11 @@ enum FileLoadStatus
 {
     FileLoadStatus_Loading,
     FileLoadStatus_LoadedButNotFinalized,
+
     // all states above need to be loading
-
     FileLoadStatus_Success,
-
     // all states below need to be errors
+
     FileLoadStatus_Error_FileNotFound,
     FileLoadStatus_Error_Other,
 };
